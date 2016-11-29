de08d7f29e6ef93fa66954536d6f1a820fadefb7
scuxiayiqian <scuxiayiqian@gmail.com>
2016-11-29 16:31:08 +0800

    register page : upload user profile

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $cordovaCamera, $cordovaActionSheet) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -56,6 +56,7 @@ angular.module('starter.controllers', [])
     $scope.reserveform.hide();
   };
 
+  $scope.profileSrc = "img/noprofile.png";
   // Open the reserve modal
   $scope.reserve = function() {
     $scope.reserveform.show();
@@ -70,6 +71,49 @@ angular.module('starter.controllers', [])
     $timeout(function() {
       $scope.closeReserve();
     }, 1000);
+  };
+
+  // 上传头像
+  $scope.openPicChooser = function() {
+    var actionSheetOptions = {
+      title: '上传头像',
+      buttonLabels: ['相机', '从图库选择'],
+      addCancelButtonWithLabel: '取消',
+      androidEnableCancelButton: true
+    };
+    $cordovaActionSheet.show(actionSheetOptions).then(function (btnIndex) {
+      var imageSource;
+      if(btnIndex == 1){
+        imageSource = Camera.PictureSourceType.CAMERA;
+      }
+      else if(btnIndex == 2){
+        imageSource = Camera.PictureSourceType.PHOTOLIBRARY;
+      }
+      else{
+        return;
+      }
+      var cameraOptions = {
+        //这些参数可能要配合着使用，比如选择了sourcetype是0，destinationtype要相应的设置
+        quality: 80,                                            //相片质量0-100
+        destinationType: Camera.DestinationType.DATA_URL,        //返回类型：DATA_URL= 0，返回作为 base64 編碼字串。 FILE_URI=1，返回影像档的 URI。NATIVE_URI=2，返回图像本机URI (例如，資產庫)
+        sourceType: imageSource,             //从哪里选择图片：PHOTOLIBRARY=0，相机拍照=1，SAVEDPHOTOALBUM=2。0和1其实都是本地图库
+        allowEdit: true,                                        //在选择之前允许修改截图
+        encodingType:Camera.EncodingType.JPEG,                   //保存的图片格式： JPEG = 0, PNG = 1
+        targetWidth: 200,                                        //照片宽度
+        targetHeight: 200,                                       //照片高度
+        mediaType:0,                                             //可选媒体类型：圖片=0，只允许选择图片將返回指定DestinationType的参数。 視頻格式=1，允许选择视频，最终返回 FILE_URI。ALLMEDIA= 2，允许所有媒体类型的选择。
+        cameraDirection:0                                      //枪后摄像头类型：Back= 0,Front-facing = 1
+      };
+
+      $cordovaCamera.getPicture(cameraOptions).then(function(imageData) {
+        
+        $scope.profileSrc = 'data:image/jpeg;base64,' + imageData;
+
+      }, function(err) {
+        // error
+        console.log("get pic err");
+      });
+    });
   };
 })
 
